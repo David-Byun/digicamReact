86661df09aedb110d2d842f10ce1825ba661b0ed
David Byun <byundavid@naver.com>
2023-03-27 16:30:32 +0900

    0327 react 수업

---
+++ smokingzone/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Main from './Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ smokingzone/src/Main.js
@@ -0,0 +1,11 @@
+import SplitScreen from './SplitScreen';
+
+function Main() {
+  return (
+    <div>
+      <SplitScreen />
+    </div>
+  );
+}
+
+export default Main;
